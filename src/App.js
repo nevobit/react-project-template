c56ef767ef194793e8c160ca7ec0f1496d25b1d2
Nestor Mosquera <yunsde18@gmail.com>
2022-02-09 13:27:05 -0500

    first iteration

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ function App() {
   return (
     <div>
         REAL VISION APP
+        <a href='/hola'>HOla MUndo</a>
+        <button>Boton</button>
+        <input type="text" name="" id="" />
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <select name="" id=""></select>
     </div>
   );
 }
